da892faa2879c38751251a7110f8bf317f02d14c
thabang56R <thabang56@hotmail.com>
2025-12-23 07:20:45 +0200

    Fix API baseURL to use Vite env variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,30 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "import.meta.env.VITE_API_URL",
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
-// Fetch all restaurants
-export const fetchRestaurants = async () => {
-  try {
-    const res = await API.get("/restaurants");
-    return res.data;
-  } catch (err) {
-    console.error("Error fetching restaurants:", err);
-    throw err;
-  }
-};
-
-// Fetch dishes by restaurant
-export const fetchDishes = async (restaurantId) => {
-  try {
-    const res = await API.get(`/dishes/${restaurantId}`);
-    return res.data;
-  } catch (err) {
-    console.error("Error fetching dishes:", err);
-    throw err;
-  }
-};
 
 export default API;
